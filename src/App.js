482ac74f7cc69a9b2d3218c29ffecbde35089d25
Wagner Andrade <wsawebmaster@yahoo.com.br>
2026-06-19 19:03:07 -0300

    feat: adicionar Context API para estado compartilhado
    - Melhora manutenção do código
    - Aumenta legibilidade dos componentes
    - Reduz prop drilling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+
+import { Counter } from './components/counter.component';
+
+function Header({ title, subtitle }) {
+  return (
+    <div>
+      <h1>{title}</h1>
+      <p>{subtitle}</p>
+      <hr />
+      <Counter />
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header 
+      title="Título da Página" 
+      subtitle="Subtítulo da Página"
+      />
     </div>
   );
 }
